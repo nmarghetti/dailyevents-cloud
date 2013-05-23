a4319e24756a42964b45b2bdc6d112d1750e3f86
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-05-23 23:02:15 +0200

    Automatically redirect dev website to prod.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
 export PATH=~/.pythonbrew/pythons/Python-2.7.3/bin:$PATH
+
 set -- "${1:-dev}"
-parse deploy dailyevents-$1
+
+if [ "$1" == "dev" ]; then
+  mv public/index.html public/index-prod.html
+  cp public/index-dev.html public/index.html
+  parse deploy dailyevents-$1
+  rm public/index.html
+  mv public/index-prod.html public/index.html
+else
+  parse deploy dailyevents-$1
+fi
