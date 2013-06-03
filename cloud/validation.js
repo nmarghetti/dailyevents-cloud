8849b973294223d68a112b46dad714acff67b952
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-06-03 22:56:49 +0200

    Avoid sending multiple success/error reponses after validation.

--- cloud/validation.js
+++ cloud/validation.js
@@ -3,59 +3,75 @@ var _  = require('underscore');
 var beforeSave = Parse.Cloud.beforeSave;
 
 beforeSave('Client', function(request, response) {
+  var errors = [];
   _.each(['environment'], function(field) {
-    ensureContains(request, response, field);
+    ensureContains(request, field, errors);
   });
-  response.success();
+  if (errors.length == 0)
+    response.success();
+  else
+    response.error(errors);
 });
 
 beforeSave('Group', function(request, response) {
+  var errors = [];
   _.each(['code', 'name'], function(field) {
-    ensureContains(request, response, field);
+    ensureContains(request, field, errors);
   });
   _.each({ 'code' : 15, 'name' : 30 }, function(maxLength, field) {
-    ensureMaxLength(request, response, field, maxLength);
+    ensureMaxLength(request, field, maxLength, errors);
   });
-  response.success();
+  if (errors.length == 0)
+    response.success();
+  else
+    response.error(errors);
 });
 
 beforeSave('Status', function(request, response) {
-  _.each(['clientId', 'groupId', 'date', 'participant', 'reply', 'timestamp', 'timezone'], function(field) {
-    ensureContains(request, response, field);
+  var errors = [];
+  _.each(['groupId', 'date', 'participant', 'reply', 'timestamp', 'timezone'], function(field) {
+    ensureContains(request, field, errors);
   });
   _.each({ 'participant' : 20, 'reply' :  3 }, function(maxLength, field) {
-    ensureMaxLength(request, response, field, maxLength);
+    ensureMaxLength(request, field, maxLength, errors);
   });
   _.each({ 'date' : 8 }, function(length, field) {
-    ensureLength(request, response, field, length);
+    ensureLength(request, field, length, errors);
   });
-  response.success();
+  if (errors.length == 0)
+    response.success();
+  else
+    response.error(errors);
 });
 
 beforeSave('Comment', function(request, response) {
-  _.each(['clientId', 'groupId', 'date', 'participant', 'comment', 'timestamp', 'timezone'], function(field) {
-    ensureContains(request, response, field);
+  var errors = [];
+  _.each(['groupId', 'date', 'participant', 'comment', 'timestamp', 'timezone'], function(field) {
+    ensureContains(request, field, errors);
   });
   _.each({ 'participant' : 20, 'comment' :  70 }, function(maxLength, field) {
-    ensureMaxLength(request, response, field, maxLength);
+    ensureMaxLength(request, field, maxLength, errors);
   });
   _.each({ 'date' : 8 }, function(length, field) {
-    ensureLength(request, response, field, length);
+    ensureLength(request, field, length, errors);
   });
-  response.success();
+  if (errors.length == 0)
+    response.success();
+  else
+    response.error(errors);
 });
 
-ensureContains = function(request, response, key) {
+ensureContains = function(request, key, errors) {
   if (!request.object.has(key))
-    response.error(key + ' is missing');
+    errors.push(key + ' is missing');
 };
 
-ensureLength = function(request, response, key, length) {
+ensureLength = function(request, key, length, errors) {
   if (request.object.get(key).length != length)
-    response.error(key + ' must have exactly' + length + ' characters');
+    errors.push(key + ' must have exactly' + length + ' characters');
 };
 
-ensureMaxLength = function(request, response, key, maxLength) {
+ensureMaxLength = function(request, key, maxLength, errors) {
   if (request.object.get(key).length > maxLength)
-    response.error(key + ' cannot have more than ' + maxLength + ' characters');
+    errors.push(key + ' cannot have more than ' + maxLength + ' characters');
 };
