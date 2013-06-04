b50e9d60b176ce9e78763533c88829a6a0a1dfc1
Tiago Fernandez <tiago.fernandez@gmail.com>
2013-06-04 23:05:14 +0200

    Automatically minify mobile.js prior to deploying.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ export PATH=~/.pythonbrew/pythons/Python-2.7.3/bin:$PATH
 
 set -- "${1:-dev}"
 
+minifyjs public/js/mobile.js
+
 if [ "$1" == "dev" ]; then
   mv public/index.html public/index-prod.html
   cp public/index-dev.html public/index.html
